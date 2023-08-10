cbb961abba12f0b6fa799481c8c13ca3c8ec9efe
ritacc12 <rita20920@gmail.com>
2023-08-10 17:00:33 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('express app for movies')
+  res.redirect('/movies')
+})
+
+//將根路徑 / redirect 到 /movies ，專案規格中，網站的首頁會直接導向電影清單
+app.get('/movies', (req, res) => {
+  res.send('listing movies')
+})
+
+app.get('/movie/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`read movie: ${id}`)
 })
 
 app.listen(port, () => {
