36188c87ec8523cc095f46e645724434fc8f5eb2
ritacc12 <rita20920@gmail.com>
2023-08-11 16:25:31 +0800

    feat: add search funtion to listing

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ app.get('/', (req, res) => {
 
 //將根路徑 / redirect 到 /movies ，專案規格中，網站的首頁會直接導向電影清單
 app.get('/movies', (req, res) => {
-  res.render('index', {movies, BASE_IMG_URL})
+  const keyword = req.query.search?.trim()
+  const matchedMovies = keyword ? movies.filter((mv) =>
+    Object.values(mv).some((property) => {
+      if (typeof property === 'string') {
+        return property.toLowerCase().includes(keyword.toLowerCase())
+      }
+      return false
+    })
+  ) : movies
+  res.render('index', { movies: matchedMovies, BASE_IMG_URL, keyword })
 })
 
 app.get('/movie/:id', (req, res) => {
